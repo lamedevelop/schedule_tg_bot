802c75cf599066ead806d3013662abbb48df1b79
oleggr <oleg.gr@outlook.com>
2020-08-31 16:38:30 +0300

    Cli controller implemented

--- CliController.py
+++ CliController.py
@@ -3,7 +3,15 @@ import importlib
 
 
 class CliController:
+    """
+    Class CliController to run managers from command line
+    Usage: 
+        python3 CliController.py --manager={ManagerName without 'Manager'} --action={Action name}
+    """
 
+
+    managerName = ""
+    actionName = ""
 
     manager = ""
     action = ""
@@ -14,17 +22,26 @@ class CliController:
         parser.add_argument("--manager")
         parser.add_argument("--action")
         args = parser.parse_args()
-        self.manager= args.manager + "Manager"
-        self.action = args.action
-        print(self.manager, self.action)
+        self.managerName= args.manager + "Manager"
+        self.actionName = args.action
 
 
-    def getController(self):
-        i = importlib.import_module(self.manager)
-        i.BotManager.sayHello(self)
+    def getManager(self):
+        foo = importlib.import_module(self.managerName)
+        self.manager = getattr(foo, self.managerName)
+
+
+    def runAction(self):
+        self.action = getattr(self.manager, self.actionName)
+        self.action(self.manager)
+
+
+    def run(self):
+        self.parseCli()
+        self.getManager()
+        self.runAction()
 
 
 if __name__ == '__main__':
     cli = CliController()
-    cli.parseCli()
-    cli.getController()
+    cli.run()
