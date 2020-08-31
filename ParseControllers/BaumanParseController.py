fd5168218b463b9220bf988f878b8f6fb9fae732
zoglam <anzoor1998@gmail.com>
2020-08-31 23:08:12 +0300

    фикс под конец создания json

--- ParseControllers/BaumanParseController.py
+++ ParseControllers/BaumanParseController.py
@@ -8,17 +8,14 @@ from ParseControllers.ParseController import ParseController
 
 class BaumanParseController(ParseController):
 
-
     #url = 'https://students.bmstu.ru/schedule/201c396c-8610-11ea-959d-005056960017'
     url = 'https://students.bmstu.ru/schedule/d2e34cf6-4aee-11e9-b081-005056960017'
-
 
     def __init__(self):
         pass
 
-
     def run(self):
-        
+
         jsonOUT = getJson(parse(self.url))
         # print(jsonOUT)
 
@@ -27,52 +24,52 @@ class BaumanParseController(ParseController):
             f.write(x)
         f.close()
 
-
     def parse(self, link):
-        try:
-            page = requests.get(link)
-        except:
-            print("Connection error")
-            return
+        page = requests.get(link)
+        if page.status_code == 200:
+            d = {
+                "div": r'<div class=\"col-md-6 hidden-xs\">(.+?)</div>',
+                "tr": r'<tr(.+?)</tr>',
+                "td": r'<td(.+?)</td>',
+                "day": r'<strong>(.+?)</strong>',
+                "time": r'class=\"bg-grey text-nowrap\">(.+?)$',
+                "doublepair": r'colspan=\"2\">(.*?)$',
+                "singlepair": r'class=\".+?\">(.+?)$',
+                "toArray": r'<i>(.*?)</i> <span>(.*?)</span> <i>(.*?)</i> <i>(.*?)</i>'
+            }
 
-        d = {
-            "div": r'<div class=\"col-md-6 hidden-xs\">(.+?)</div>',
-            "tr": r'<tr(.+?)</tr>',
-            "td": r'<td(.+?)</td>',
-            "day": r'<strong>(.+?)</strong>',
-            "time": r'class=\"bg-grey text-nowrap\">(.+?)$',
-            "doublepair": r'colspan=\"2\">(.*?)$',
-            "singlepair": r'class=\".+?\">(.+?)$',
-            "toArray": r'<i>(.*?)</i> <span>(.*?)</span> <i>(.*?)</i> <i>(.*?)</i>'
-        }
-
-        for div in re.findall(d["div"], page.text.replace("\n", "")):
-            stageInTR = 1
-            for tr in re.findall(d["tr"], div):
-                stageInTD = 1
-                for td in re.findall(d["td"], tr):
-                    value = []
-                    if stageInTR == 1:
-                        value = re.findall(d["day"], td)  # День недели
-                    elif stageInTR > 2:
-                        if stageInTD == 1:
-                            value = re.findall(d["time"], td)  # Время пары
-                        else:
-                            value = re.findall(d["doublepair"], td)  # Сдвоенная
-                            if not value:
-                                value = re.findall(d["singlepair"], td)  # Пара
-                            value = re.findall(d["toArray"], str(value))  # [array]
-                            if not value:
-                                value = ['', '', '', '']  # Если ячейка пустая
-                                #value = []  # Если ячейка пустая
+            for div in re.findall(d["div"], page.text.replace("\n", "")):
+                stageInTR = 1
+                for tr in re.findall(d["tr"], div):
+                    stageInTD = 1
+                    for td in re.findall(d["td"], tr):
+                        value = []
+                        if stageInTR == 1:
+                            value = re.findall(d["day"], td)  # День недели
+                        elif stageInTR > 2:
+                            if stageInTD == 1:
+                                value = re.findall(d["time"], td)  # Время пары
                             else:
-                                value = [list(x) for x in value][0]
-                        stageInTD += 1
-                    yield value
-                stageInTR += 1
+                                value = re.findall(
+                                    d["doublepair"], td)  # Сдвоенная
+                                if not value:
+                                    value = re.findall(
+                                        d["singlepair"], td)  # Пара
+                                value = re.findall(
+                                    d["toArray"], str(value))  # [array]
+                                if not value:
+                                    # Если ячейка пустая
+                                    value = ['', '', '', '']
+                                    # value = []  # Если ячейка пустая
+                                else:
+                                    value = [list(x) for x in value][0]
+                            stageInTD += 1
+                        yield value
+                    stageInTR += 1
+        else:
+            print("Connection error")
 
-
-    def getJson(self, webpage):
+    def getJson(self, text):
         d = {
             "ПН": "Понедельник",
             "ВТ": "Вторник",
@@ -84,7 +81,7 @@ class BaumanParseController(ParseController):
         finalDict, timeArr = {}, {}
         day, time = "", ""
         pairs = []
-        for result in webpage:
+        for result in text:
             if result:
                 if len(result[0]) == 2:
                     if timeArr:
@@ -99,5 +96,7 @@ class BaumanParseController(ParseController):
                 else:
                     pairs.append(
                         list(map(lambda x: x.replace("&#160;", " "), result)))
+            if timeArr:
+                timeArr[time] = pairs
+                finalDict[day] = timeArr
         return json.dumps(finalDict, indent=4, ensure_ascii=False)
-
