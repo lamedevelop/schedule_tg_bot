bd54f80371f27c49174c4d347c89f83896449bff
oleggr <oleg.gr@outlook.com>
2020-09-05 13:08:16 +0300

    Added base structure for startup script. Added requirements file

---
+++ Scripts/startup.py
@@ -0,0 +1,16 @@
+"""
+Script that should be run at the beginning of working
+"""
+
+
+def resetDb():
+    pass
+
+def upMigrations():
+    pass
+
+def downMigrations():
+    pass
+
+if __name__=="__main__":
+    pass
