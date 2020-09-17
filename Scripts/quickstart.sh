18c13fa19e926cb336f25b0ffe1129b29e710d8f
oleggr <oleg.gr@outlook.com>
2020-09-18 01:21:13 +0300

    Quickstart updated

--- Scripts/quickstart.sh
+++ Scripts/quickstart.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-openssl req -newkey rsa:2048 -sha256 -nodes -keyout private.key -x509 -days 3650 -out cert.pem
+#openssl req -newkey rsa:2048 -sha256 -nodes -keyout private.key -x509 -days 3650 -out cert.pem
+
+mkdir Logs
 
 python3 RunManager.py --manager=Db --action=upAllMigrations
 python3 RunManager.py --manager=Db --action=resetDb
+
+echo "Update Configs/tgConfig.py before running bot"
