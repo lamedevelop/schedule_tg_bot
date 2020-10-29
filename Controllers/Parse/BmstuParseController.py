bb08ee0b4aa460a80eeb57a233e33c814b69387f
zoglam <anzoor1998@gmail.com>
2020-10-29 23:09:07 +0300

    dateFix and code formating

--- Controllers/Parse/BmstuParseController.py
+++ Controllers/Parse/BmstuParseController.py
@@ -1,5 +1,4 @@
 import re
-import requests
 from bs4 import BeautifulSoup  # https://pypi.org/project/beautifulsoup4/
 
 from Controllers.Parse.ParseController import ParseController
@@ -13,91 +12,82 @@ class BmstuParseController(ParseController):
 
     def _parse(self, group_name: str):
 
-        try:
-            get_list = requests.get(self.SCHEDULE_LIST_URL)
-        except requests.ConnectionError as e:
-            self.logger.alert(str(e))
+        get_list = self.getUrl(self.SCHEDULE_LIST_URL)
+        if get_list is None:
             return {}
 
-        if get_list.status_code == 200:
-            html_list = get_list.text.replace('\n', '')
-            soup_list = BeautifulSoup(html_list, 'html.parser')
-            a_tags = soup_list.find_all(
-                'a', class_='btn btn-sm btn-default text-nowrap')
-            group_schedule_dict = {}
+        html_list = get_list.text.replace('\n', '')
+        soup_list = BeautifulSoup(html_list, 'html.parser')
+        a_tags = soup_list.find_all(
+            'a', class_='btn btn-sm btn-default text-nowrap')
+        group_schedule_dict = {}
 
-            for i, item in enumerate(a_tags):
-                if item.get_text().strip() == group_name:
-                    self._parseTask(item, group_schedule_dict)
-                    break
-            return group_schedule_dict
-        else:
-            return {}
+        for item in a_tags:
+            if item.get_text().strip() == group_name:
+                self._parseTask(item, group_schedule_dict)
+                break
+        return group_schedule_dict
 
     def _parseTask(self, item, group_schedule_dict):
         group_name = item.get_text().strip()
-        group_schedule_url = 'https://students.bmstu.ru/%s' % item.get('href')
+        group_schedule_url = f"https://students.bmstu.ru/{item.get('href')}"
 
-        try:
-            get_group_schedule = requests.get(group_schedule_url)
-        except requests.ConnectionError as e:
-            self.logger.alert(str(e))
-            return {}
-
-        if get_group_schedule.status_code == 200:
-            week = {}
-            html_group = get_group_schedule.text.replace('\n', '')
-            soup_group = BeautifulSoup(html_group, 'html.parser')
-            day_div = soup_group.find_all(
-                'div', class_='col-md-6 hidden-sm hidden-md hidden-lg')
-
-            for div in day_div:
-                day = {}
-                day_shedule = div.find('tbody')
-                day_name = day_shedule.find('strong').get_text()
-                time_and_subject = day_shedule.find_all('tr')
-
-                for index, item in enumerate(time_and_subject):
-                    if index > 1:
-                        curr_time = item.find(
-                            'td', class_='bg-grey text-nowrap').get_text()
-                        time_groups = re.match(r'(.{5})(.{5})', curr_time)
-                        curr_time = f'{time_groups.group(1)}-{time_groups.group(2)}'
-
-                        both = item.find('td', colspan='2')
-                        subject_patt = re.compile(
-                            r'<i>(.*?)</i> <span>(.*?)</span> <i>(.*?)</i> <i>(.*?)</i>')
-
-                        if both:
-                            both_turple = subject_patt.findall(
-                                str(both).replace('\xa0', ' ').replace('Самостоятельная работа', '')
-                            )
-                            both_schedule = [list(x) for x in both_turple][0]
-
-                            subject = {
-                                'both': both_schedule if list(
-                                    filter(lambda x: bool(x), both_schedule)
-                                ) else ['']
-                            }
-                        else:
-                            numerator = item.find('td', class_='text-success')
-                            denominator = item.find('td', class_='text-info')
-                            numerator_turple = subject_patt.findall(
-                                str(numerator).replace('\xa0', ' ')
-                            )
-                            denominator_turple = subject_patt.findall(
-                                str(denominator).replace('\xa0', ' ')
-                            )
-                            subject = {
-                                'numerator': [list(x) for x in numerator_turple][0] if numerator_turple else [''],
-                                'denominator': [list(x) for x in denominator_turple][0] if denominator_turple else ['']
-                            }
-
-                        day[curr_time] = subject
-                week[day_name] = day
-            group_schedule_dict[group_name] = week
-        else:
+        get_group_schedule = self.getUrl(group_schedule_url)
+        if get_group_schedule is None:
             group_schedule_dict[group_name] = {}
+
+        week = {}
+        html_group = get_group_schedule.text.replace('\n', '')
+        soup_group = BeautifulSoup(html_group, 'html.parser')
+        day_div = soup_group.find_all(
+            'div', class_='col-md-6 hidden-sm hidden-md hidden-lg')
+
+        for div in day_div:
+            day = {}
+            day_shedule = div.find('tbody')
+            day_name = day_shedule.find('strong').get_text()
+            time_and_subject = day_shedule.find_all('tr')
+
+            for index, item in enumerate(time_and_subject):
+                if index > 1:
+                    curr_time = item.find(
+                        'td', class_='bg-grey text-nowrap').get_text()
+                    time_groups = re.match(r'(.{5})(.{5})', curr_time)
+                    curr_time = f'{time_groups.group(1)}-{time_groups.group(2)}'
+
+                    both = item.find('td', colspan='2')
+                    subject_patt = re.compile(
+                        r'<i>(.*?)</i> <span>(.*?)</span> <i>(.*?)</i> <i>(.*?)</i>')
+
+                    if both:
+                        both_turple = subject_patt.findall(
+                            str(both).replace('\xa0', ' ').replace(
+                                'Самостоятельная работа', '')
+                        )
+                        both_schedule = [list(x) for x in both_turple][0]
+
+                        subject = {
+                            'both': both_schedule if list(
+                                filter(lambda x: bool(x), both_schedule)
+                            ) else ['']
+                        }
+                    else:
+                        numerator = item.find('td', class_='text-success')
+                        denominator = item.find('td', class_='text-info')
+                        numerator_turple = subject_patt.findall(
+                            str(numerator).replace('\xa0', ' ')
+                        )
+                        denominator_turple = subject_patt.findall(
+                            str(denominator).replace('\xa0', ' ')
+                        )
+                        subject = {
+                            'numerator': [list(x) for x in numerator_turple][0] if numerator_turple else [''],
+                            'denominator': [list(x) for x in denominator_turple][0] if denominator_turple else ['']
+                        }
+
+                    day[curr_time] = subject
+            week[day_name] = day
+        group_schedule_dict[group_name] = week
 
     def __str__(self):
         return '2'
--- Controllers/Parse/ParseController.py
+++ Controllers/Parse/ParseController.py
@@ -1,8 +1,8 @@
-import json
+import json, requests
 
 from Controllers.Log.LogController import LogController
 from Controllers.File.FileController import FileController
-
+from abc import abstractmethod
 
 class ParseController:
 
@@ -14,12 +14,29 @@ class ParseController:
         filepath = f'{file_name}.json'
         FileController.writeToFile(filepath, json)
 
-    def _parse(self, group_name: str):
-        return ''
-
     def makeJson(self, group_name: str):
-        # return json.dumps(self._parse(group_name), indent=0, ensure_ascii=False)
         return str(self._parse(group_name))
 
+    def getUrl(self, url):
+        try:
+            response = requests.get(url, timeout=30)            
+        except requests.Timeout:
+            self.logger.alert(f'Timeout error, url: {url}')
+            return None
+        except requests.HTTPError as err:
+            code = err.response.status_code
+            self.logger.alert(f'Error url: {url}, code: {code}')
+            return None
+        except requests.RequestException:
+            self.logger.alert(f'Downloading error url: {url}')
+            return None
+        else:
+            return response
+    
+    @abstractmethod
+    def _parse(self, group_name: str):
+        pass
+
+    @abstractmethod
     def __str__(self):
-        return ''
+        pass
