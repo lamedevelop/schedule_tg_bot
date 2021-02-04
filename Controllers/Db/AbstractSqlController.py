d730b9e1c28de637184609768bcaec2def06c670
oleggr <oleg.gr@outlook.com>
2021-02-04 21:49:54 +0300

    db controllers updated

---
+++ Controllers/Db/AbstractSqlController.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+
+from Controllers.CliArgsController import CliArgsController
+from Controllers.Log.LogController import LogController
+
+
+class AbstractSqlController:
+
+    DB_PARAMS: dict
+
+    def __init__(self) -> None:
+        self.config = CliArgsController().getConfig()
+        self.logger = LogController()
+
+    @abstractmethod
+    def _executeQuery(self, query: str) -> int: ...
+
+    @abstractmethod
+    def _fetchResult(self) -> list: ...
+
+    @abstractmethod
+    def submitQuery(self, query: str) -> int: ...
+
+    @abstractmethod
+    def fetchQuery(self, query: str) -> list: ...
+
+    @abstractmethod
+    def makeDump(self) -> None: ...
--- Controllers/Db/SqlLiteDbController.py
+++ Controllers/Db/SqlLiteDbController.py
@@ -1,31 +1,60 @@
 import os
 import sqlite3
 
-from Controllers.Db.AbstractDbController import AbstractDbController
+from Controllers.Db.AbstractSqlController import AbstractSqlController
 
 
-class SqlLiteDbController(AbstractDbController):
+class SqlLiteDbController(AbstractSqlController):
 
     def __init__(self):
         super().__init__()
-        self.DB_MODULE = sqlite3
         self.DB_PARAMS = {
             'database': self.config.DB_FILENAME
         }
+        self.conn = sqlite3.connect(**self.DB_PARAMS)
+        self.cursor = self.conn.cursor()
+
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
+
+    def _executeQuery(self, query: str) -> int:
+        self.cursor.execute(query)
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    def _fetchResult(self) -> list:
+        rows = self.cursor.fetchall()
+        return [row for row in rows]
+
+    def submitQuery(self, query: str) -> int:
+        try:
+            record_id = self._executeQuery(query)
+            return record_id
+        except Exception as error:
+            self.logger.alert(f'Error while connecting to SQLITE db: {error}')
+            print('Problem query: ', query)
+
+    def fetchQuery(self, query: str) -> list:
+        try:
+            self._executeQuery(query)
+            result = self._fetchResult()
+            return result
+        except Exception as error:
+            self.logger.alert(f'Error while connecting to SQLITE db: {error}')
+            print('Problem query: ', query)
 
     def makeDump(self):
         try:
-            self._openConnection()
             with open(self.config.DUMP_FILENAME, 'w') as file:
                 for line in self.conn.iterdump():
                     file.write('%s\n' % line)
-            self._closeConnection()
         except Exception as error:
-            self.logger.alert(f'Error while dumping db: {error}')
+            self.logger.alert(f'Error while dumping SQLITE db: {error}')
 
     def dropDb(self):
         try:
             os.remove(self.config.DB_FILENAME)
             self.logger.info('DB was deleted')
         except Exception as e:
-            self.logger.alert('Error while deleting db: {}'.format(e))
+            self.logger.alert('Error while deleting SQLITE db: {}'.format(e))
